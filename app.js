cb59e7061d70b72b76280a05e32c3af4c74c95f0
Jiyun <baejiyun01@gmail.com>
2021-02-20 06:02:19 +0900

    update changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { isObject } = require('util');
 
 const messenger = require('socket.io')();
 
@@ -7,7 +8,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
        res.sendFile(path.join(__dirname, "index.html"));
@@ -21,4 +22,28 @@ const server = app.listen(port, () => {
        console.log(`app is running on ${port}`);
 });
 
-messenger.attath(server);
+// messenger is the connection manager - like a switchboard operator
+messenger.attach(server);
+
+// socket is the individual connection - the caller
+messenger.on('connection', (socket) => {
+       console.log(`a user connected: ${socket.id}`);
+
+       // send the connected user their assigned ID
+       socket.emit('connected', { sID: `${socket.id}`, message: 'new connection'});
+
+       socket.on('chatmessage', function(msg) {
+              console.log(msg);
+              
+              messenger.emit('message', { id: socket.id, message: msg });
+       });
+
+       socket.on('add-user', function(user){
+              users.push({id: socket.id, name: user });
+              io.emit('update-users', users);
+       })
+
+       socket.on('disconnect', () => {
+              console.log('a user has disconnected');
+       })
+});
